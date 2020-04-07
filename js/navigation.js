6a2d85fde5328ef4f1815f9ed1a4306bef4177fd
alproddev <a@adamleon.xyz>
2020-04-07 14:40:08 -0400

    v3.4
    See More Results Page

--- js/navigation.js
+++ js/navigation.js
@@ -9,9 +9,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("shopify-section-header").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-63px";
+    document.getElementById("shopify-section-header").style.top = "-63px";
   }
   prevScrollpos = currentScrollPos;
 };
